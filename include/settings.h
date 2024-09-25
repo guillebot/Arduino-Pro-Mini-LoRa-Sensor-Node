f4f09f86a5e3cf921dd1b83a41fe3f6355aa18ed
guillermo.schimmel <guillermo.schimmel@cwc.com>
2024-09-25 11:35:57 -0300

    30 minutes messages

--- include/settings.h
+++ include/settings.h
@@ -1,3 +1,6 @@
-#define LORA_TX_INTERVAL 300
+// Todas las pruebas fueron con 300, es decir 5 minutos
+//#define LORA_TX_INTERVAL 300
+// Para produccion lo paso a 30 minutos
+#define LORA_TX_INTERVAL 1800
 //#define LORA_TX_INTERVAL 60 // Para debug! Volver luego al 300 y después mas.
 #define LMIC_LORA_SF DR_SF7
